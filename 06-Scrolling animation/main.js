8f90be02ed2575e4715b16d3ac7ab24c6297a9e5
Francis Kintungi <franciskintungi@gmail.com>
2022-01-13 14:07:55 +0300

    Scroll animation

---
+++ 06-Scrolling animation/main.js
@@ -0,0 +1,29 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    // console.log(window.innerHeight )
+    /*this is used to check the trigger point but since 
+    the we want the event to trigger a few point before 
+    the position of the box, we edit the value to 
+    return a bit smalller number, see below*/
+    // console.log(window.innerHeight / 5 * 4.5)
+
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach((box) => {
+        const rect = box.getBoundingClientRect();
+        //getting the rectangle properties of each box
+        // console.log(rect);
+        boxTop = rect.top;
+
+        if (boxTop < triggerBottom) {//If a box goes above the trigger point
+            box.classList.add("show");
+        } else {
+            box.classList.remove("show");
+        }
+    })
+}
